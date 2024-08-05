d43918beda27aff45562d7d877162fe38165712e
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-08-05 12:38:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Dependencies
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-500">
+      <h2 className="text-center text-4xl font-bold text-blue-900">
+        helljhbvcgfxo
+      </h2>
+    </div>
+  );
+};
+
+export default App;
