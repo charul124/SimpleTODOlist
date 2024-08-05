1d681eef6ad20fdcb6563da1e6bbbc5aee6f32e3
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-08-05 12:43:21 +0000

    somechnages

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-500">
-      <h2 className="text-center text-4xl font-bold text-blue-900">
-        helljhbvcgfxo
-      </h2>
+      <h2 className="text-center text-4xl font-bold text-blue-900">helhbo</h2>
     </div>
   );
 };
